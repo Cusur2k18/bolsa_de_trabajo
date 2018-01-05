27bf2d39113c07c3a3808e2faf2770b4b65f0559
Antonio <antonio.miranda9696@gmail.com>
2018-01-05 13:31:58 -0600

    Installed devise, bootstrap, jquery, added user and companies models, configured home view and controller

--- Gemfile
+++ Gemfile
@@ -30,6 +30,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+# User defined gems.
+gem 'bootstrap-sass', '~> 3.3.7'
+gem 'devise'
+gem 'formtastic', '~> 3.0'
+gem 'jquery-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
---
+++ app/models/student.rb
@@ -0,0 +1,7 @@
+class Student < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  enum state: [:desempleado, :proceso, :contratado]
+end
