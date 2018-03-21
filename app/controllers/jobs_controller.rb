8cbb19cb30d1835851dee749b17494e3c71bf0c2
Antonio <antonio.miranda9696@gmail.com>
2018-03-21 17:47:00 -0600

    Fixed all CRUD on jobs, and fixed a bug on new.html.erb on phone_numbers

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,8 @@ class JobsController < ApplicationController
 	before_action :is_company
 
 	def index
+		@company = current_user.roleable
+		@jobs = Job.where(company_id: @company.id)
 	end
 
 	def new
@@ -14,11 +16,42 @@ class JobsController < ApplicationController
 		@company = current_user.roleable
 		@company.jobs << @job
 		if @job.save
+			flash[:notice] = "Se ha guardado correctamente la vacante"
 			redirect_to jobs_path
 		else
 			flash[:alert] = t('warnings.something_went_wrong')
 			render :new 
 		end
+	end
+
+	def show
+		@job = Job.find(params[:id])
+	end
+
+
+	def edit
+		@job = Job.find(params[:id])
+		if is_owner(@job)
+			@job
+		else
+			flash[:alert] = "No se ha podido editar esta vacante"
+			redirect_to jobs_path
+		end
+	end
+
+	def update
+		@job = Job.find(params[:id])
+		if is_owner(@job)
+			@job.update_attributes(jobs_params)
+			flash[:notice] = "Se ha actualizado correctamente la vacante"
+			redirect_to jobs_path
+		else
+			flash[:alert] = "No se ha podido actualizar esta vacante"
+			redirect_to jobs_path
+		end
+	end
+
+	def destroy
 	end
 
 	private
@@ -31,7 +64,15 @@ class JobsController < ApplicationController
 		end
 	end
 
+	def is_owner(job)
+		if job.company_id == current_user.id
+			true
+		else
+			false
+		end
+	end
+
 	def jobs_params
-		params.require(:job).permit(:category, :workday_schedule, :contract_type, :salary, :name, :age_requierement, :gender, :schooling_level, :work_experience, :specialized_knowledge, :aptitudes, :job_duration, :workday_type)
+		params.require(:job).permit(:category, :workday_schedule_start, :workday_schedule_end, :contract_type, :salary, :name, :age_requierement, :gender, :schooling_level, :work_experience, :specialized_knowledge, :aptitudes, :job_duration, :workday_type)
 	end
 end
